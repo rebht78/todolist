dc183278773cf8814f18bef76fb860404b7834c9
Sohel Shaikh <rebht78@gmail.com>
2017-11-06 22:16:55 +0530

    Added index.html, assets (CSS, jQuery library and javascript file)

---
+++ assets/js/todos.js
@@ -0,0 +1,39 @@
+// assigning each li element with a click event handler
+$("ul").on("click", "li", function() {
+	// toggling the class completed when the user clicked on li
+	$(this).toggleClass("completed");	
+});
+
+// assigning each span element with a click event handler
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$(".todo_input").keypress(function(event){
+	if (event.which === 13)
+	{
+		var todo_input = $(this).val();
+
+		$(this).val("");
+
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+todo_input+"</li>");
+	}
+});
+
+$(".fa-plus").click(function() {
+	$(".todo_input").fadeToggle();
+});
+
+$(".fa-question-circle-o").click(function() {
+	alert("Help for this to do list coming soon!");
+});
+
+$(".fa-pencil").click(function() {
+	var headingContent = prompt("Enter To Do List Heading for your reference", "To Do List 1");
+	alert(headingContent);
+	$("#headerContent").val(headingContent);
+
+});
